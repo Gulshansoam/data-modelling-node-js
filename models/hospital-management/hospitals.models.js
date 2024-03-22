1e26594b3f9497b91779c420f693b688c5aae889
Gulshansoam <gulshansoam317@gmail.com>
2024-03-22 14:12:05 +0530

    model-done

--- models/hospital-management/hospitals.models.js
+++ models/hospital-management/hospitals.models.js
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({},{timestamps: true})
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+    },
+    pincode: {
+      type: String,
+      required: true,
+    },
+    specializedIn: {
+      type: [
+        {
+          type: String,
+        },
+      ],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Hospital = mongoose.model("Hospital", hospitalSchema)
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
